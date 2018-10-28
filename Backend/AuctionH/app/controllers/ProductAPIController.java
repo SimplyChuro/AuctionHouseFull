f686f21c0a897df0c969a73b38f0dbf55707a5e6
SimplyChuro <ribiczijah@gmail.com>
2018-10-28 11:00:04 +0100

    product API additions/etc..

--- Backend/AuctionH/app/controllers/ProductAPIController.java
+++ Backend/AuctionH/app/controllers/ProductAPIController.java
@@ -24,25 +24,25 @@ public class ProductAPIController extends Controller{
 	}
 	
 	//API GET top 5 bidders of a product
-		public Result productBidderList(Long id) {
-			try {
-				List<UserBid> bidList = UserBid.find.query().where().conjunction()
-						.eq("product_bid_reference_id", id )
-						.endJunction()
-						.orderBy("userBidAmount dsc")
-				        .setFirstRow(0)
-				        .setMaxRows(5)
-				        .findList();
-				
-				ArrayNode userProductBidArray = Json.newArray();
-				for(UserBid bid : bidList) {
-					userProductBidArray.add(bid.toJsonProduct());
-				}
-				
-				return ok(Json.toJson(userProductBidArray));
-			}catch(Exception e){
-				return notFound(views.html._404.render());
+	public Result productBidderList(Long id) {
+		try {
+			List<UserBid> bidList = UserBid.find.query().where().conjunction()
+					.eq("product_bid_reference_id", id )
+					.endJunction()
+					.orderBy("userBidAmount dsc")
+			        .setFirstRow(0)
+			        .setMaxRows(5)
+			        .findList();
+			
+			ArrayNode userProductBidArray = Json.newArray();
+			for(UserBid bid : bidList) {
+				userProductBidArray.add(bid.toJsonProduct());
 			}
+			
+			return ok(Json.toJson(userProductBidArray));
+		}catch(Exception e){
+			return notFound(views.html._404.render());
 		}
+	}
 	
 }
